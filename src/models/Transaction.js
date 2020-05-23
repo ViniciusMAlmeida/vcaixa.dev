e2e61f9d7df1eaeee996ef039f1e2acb2ea8330c
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2020-05-23 18:00:25 -0300

    corigindo nome do campo de data

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -11,7 +11,7 @@ const TransactionSchema = new mongoose.Schema({
     TransactionType: {
         type: TransactionTypeDTO
     },
-    data: {
+    date: {
         type: Date,
         default: Date.now()
     },
