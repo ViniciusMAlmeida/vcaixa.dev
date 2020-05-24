11fd85a8bd7eb06341b72b43f5a19ce57b29abb9
Vinicius M. Almeida <vinicius.sec21@gmail.com>
2020-05-24 00:02:05 -0300

    habilitando CORS

--- src/server.js
+++ src/server.js
@@ -4,9 +4,11 @@ const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const mongoConfig = require('./config/mongoConfig')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(process.env.DB_NAME, mongoConfig)
 
